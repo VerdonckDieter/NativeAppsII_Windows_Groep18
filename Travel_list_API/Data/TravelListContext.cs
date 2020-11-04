a67a56b4ba8a0c0c4921d6e47e11d8cde465533d
Dieter Verdonck <dieter.verdonck@student.hogent.be>
2020-11-04 15:59:40 +0100

    Add TravelList context
    Co-authored-by: helderweirtthibault <thibault.helderweirt@student.hogent.be>

---
+++ Travel_list_API/Data/TravelListContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Travel_list_API.Models;
+
+namespace Travel_list_API.Data
+{
+    public class TravelListContext : DbContext
+    {
+        public DbSet<TravelList> TravelLists { get; set; }
+
+
+        public TravelListContext(DbContextOptions<TravelListContext> options) : base(options) { }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            //necessary for supporting the Add-Migration command from Pack Man Console
+            var connectionstring = @"Server=(localdb)\MSSQLLocalDB;Database=TravelListDB;Trusted_Connection=True;MultipleActiveResultSets=true";
+            optionsBuilder.UseSqlServer(connectionstring);
+        } 
+    }
+}
