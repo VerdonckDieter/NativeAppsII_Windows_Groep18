14673f705454941d2aee6603c60bae55bbc5f024
Dieter Verdonck <dieter.verdonck@student.hogent.be>
2020-11-14 17:11:12 +0100

    Add viewmodel/command
    Co-authored-by: helderweirtthibault <thibault.helderweirt@student.hogent.be>

---
+++ NativeAppsII_Windows_Groep18/ViewModel/Commands/AddTravelListCommand.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace NativeAppsII_Windows_Groep18.ViewModel.Commands
+{
+    public class AddTravelListCommand : ICommand
+    {
+        #region Properties
+        public event EventHandler CanExecuteChanged;
+
+        public TravelListViewModel ViewModel { get; set; }
+        #endregion
+
+        #region Constructor
+        public AddTravelListCommand(TravelListViewModel vm)
+        {
+            this.ViewModel = vm;
+        }
+        #endregion
+
+        #region Methods
+        public bool CanExecute(object parameter) => true;
+
+        public void Execute(object parameter) => AddTravelList(parameter);
+
+        private async void AddTravelList(object t) => await ViewModel.AddTravelList(); 
+        #endregion
+    }
+}
---
+++ NativeAppsII_Windows_Groep18/ViewModel/TravelListViewModel.cs
@@ -0,0 +1,57 @@
+﻿using NativeAppsII_Windows_Groep18.DataModel;
+using NativeAppsII_Windows_Groep18.ViewModel.Commands;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Threading.Tasks;
+using Windows.Web.Http;
+
+namespace NativeAppsII_Windows_Groep18.ViewModel
+{
+    public class TravelListViewModel
+    {
+        #region Properties
+        public AddTravelListCommand AddCommand { get; set; }
+
+        public ObservableCollection<TravelList> TravelLists { get; set; }
+        #endregion
+
+        #region Constructor
+        public TravelListViewModel()
+        {
+            TravelLists = new ObservableCollection<TravelList>();
+            LoadData();
+            AddCommand = new AddTravelListCommand(this);
+        }
+        #endregion
+
+        #region Methods
+        private async void LoadData()
+        {
+            HttpClient client = new HttpClient();
+            var json = await client.GetStringAsync(new Uri("http://localhost:5000/api/travelLists/"));
+            var travelLists = JsonConvert.DeserializeObject<IList<TravelList>>(json);
+            foreach (var travelList in travelLists)
+            {
+                TravelLists.Add(travelList);
+            }
+        }
+
+        public async Task AddTravelList()
+        {
+            //Nog travelList aanmaken maar weet nog niet hoe
+            var travelList = new TravelList();
+            var travelListJson = JsonConvert.SerializeObject(travelList);
+
+            HttpClient client = new HttpClient();
+            var res = await client.PostAsync(new Uri("http://localhost:5000/api/travelLists/"), new HttpStringContent(travelListJson, Windows.Storage.Streams.UnicodeEncoding.Utf8, "application/json"));
+
+            if (res.IsSuccessStatusCode)
+            {
+                TravelLists.Add(JsonConvert.DeserializeObject<TravelList>(res.Content.ToString()));
+            }
+        } 
+        #endregion
+    }
+}
