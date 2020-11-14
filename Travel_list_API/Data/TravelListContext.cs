00b4bbbd1759a8ab81f050b89308793b12d5c7bf
Dieter Verdonck <dieter.verdonck@student.hogent.be>
2020-11-14 17:08:04 +0100

    Add test data
    Co-authored-by: helderweirtthibault <thibault.helderweirt@student.hogent.be>

--- Travel_list_API/Data/TravelListContext.cs
+++ Travel_list_API/Data/TravelListContext.cs
@@ -14,11 +14,41 @@ namespace Travel_list_API.Data
 
         public TravelListContext(DbContextOptions<TravelListContext> options) : base(options) { }
 
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        /*protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             //necessary for supporting the Add-Migration command from Pack Man Console
             var connectionstring = @"Server=(localdb)\MSSQLLocalDB;Database=TravelListDB;Trusted_Connection=True;MultipleActiveResultSets=true";
             optionsBuilder.UseSqlServer(connectionstring);
-        } 
+        }*/
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            //base.OnModelCreating(builder);
+
+            builder.Entity<TravelList>()
+                .HasMany(t => t.Items)
+                .WithOne()
+                .IsRequired()
+                .HasForeignKey("TravelListId");
+
+            builder.Entity<TravelList>().HasData(
+                new TravelList() { Id = 1, Name = "Test1", Date = DateTime.Now },
+                new TravelList() { Id = 2, Name = "Test2", Date = DateTime.Now },
+                new TravelList() { Id = 3, Name = "Test3", Date = DateTime.Now },
+                new TravelList() { Id = 4, Name = "Test4", Date = DateTime.Now },
+                new TravelList() { Id = 5, Name = "Test5", Date = DateTime.Now }
+                );
+
+            builder.Entity<Item>().HasData(
+                new { Id = 1, Name = "Item 1", Added = false, Amount = 1, Category = Item.ItemCategory.Kledij, TravelListId = 1 },
+                new { Id = 2, Name = "Item 2", Added = true, Amount = 2, Category = Item.ItemCategory.Kledij, TravelListId = 1 },
+                new { Id = 3, Name = "Item 3", Added = true, Amount = 5, Category = Item.ItemCategory.Kledij, TravelListId = 2 },
+                new { Id = 4, Name = "Item 4", Added = false, Amount = 3, Category = Item.ItemCategory.Kledij, TravelListId = 2 },
+                new { Id = 5, Name = "Item 5", Added = false, Amount = 1, Category = Item.ItemCategory.Kledij, TravelListId = 3 },
+                new { Id = 6, Name = "Item 6", Added = true, Amount = 8, Category = Item.ItemCategory.Kledij, TravelListId = 4 },
+                new { Id = 7, Name = "Item 7", Added = false, Amount = 6, Category = Item.ItemCategory.Kledij, TravelListId = 4 },
+                new { Id = 8, Name = "Item 8", Added = true, Amount = 9, Category = Item.ItemCategory.Kledij, TravelListId = 5 }
+                );
+        }
     }
 }
