bb1e15f335a1f7a63f82db364429d550a6eb04c3
Dieter Verdonck <dieter.verdonck@student.hogent.be>
2020-11-28 22:38:59 +0100

    Add separate navigation page
    Co-authored-by: helderweirtthibault <thibault.helderweirt@student.hogent.be>

---
+++ NativeAppsII_Windows_Groep18/View/Navigation.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace NativeAppsII_Windows_Groep18.View
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class Navigation : Page
+    {
+        public Navigation()
+        {
+            this.InitializeComponent();
+            this.Content.Navigate(typeof(MasterDetail));
+        }
+
+        private void NavView_ItemInvoked(NavigationView sender, NavigationViewItemInvokedEventArgs args)
+        {
+            switch (args.InvokedItem.ToString())
+            {
+                case "Home":
+                    Content.Navigate(typeof(MasterDetail));
+                    break;
+                case "Add TravelList":
+                    Content.Navigate(typeof(AddTravelListView));
+                    break;
+                case "Logout":
+                    Content.Navigate(typeof(MainPage));
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
