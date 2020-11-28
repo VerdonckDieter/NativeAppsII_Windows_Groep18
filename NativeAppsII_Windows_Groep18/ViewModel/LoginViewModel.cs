9bf5aba9071fb81076d07d8ff1d75b2c739c6de2
Dieter Verdonck <dieter.verdonck@student.hogent.be>
2020-11-28 22:40:53 +0100

    Add login viewmodel
    Co-authored-by: helderweirtthibault <thibault.helderweirt@student.hogent.be>

---
+++ NativeAppsII_Windows_Groep18/ViewModel/LoginViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+
+namespace NativeAppsII_Windows_Groep18.ViewModel
+{
+    class LoginViewModel
+    {
+        public async Task Login(string mail, string pw)
+        {
+            HttpClient client = new HttpClient();
+        }
+    }
+}
