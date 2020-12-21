59f8dd783736c49479b7c4f5b9f7d2eb2240bedb
Dieter Verdonck <dieter.verdonck@student.hogent.be>
2020-12-22 00:06:24 +0100

    Remove unused imports
    Co-authored-by: helderweirtthibault <thibault.helderweirt@student.hogent.be>

--- NativeAppsII_Windows_Groep18/Model/TravelList.cs
+++ NativeAppsII_Windows_Groep18/Model/TravelList.cs
@@ -1,12 +1,8 @@
 ﻿using NativeAppsII_Windows_Groep18.Model;
 using System;
-using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
-using System.Linq;
 using System.Runtime.CompilerServices;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace NativeAppsII_Windows_Groep18.DataModel
 {
