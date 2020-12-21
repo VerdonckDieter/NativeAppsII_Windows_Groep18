3104d8f18f64878b3e4410fc808d394d016bc0b4
Dieter Verdonck <dieter.verdonck@student.hogent.be>
2020-12-22 00:05:31 +0100

    Add repositories to StartUp
    Co-authored-by: helderweirtthibault <thibault.helderweirt@student.hogent.be>

--- Travel_list_API/Startup.cs
+++ Travel_list_API/Startup.cs
@@ -1,18 +1,13 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Travel_list_API.Data;
 using Travel_list_API.Data.Repositories;
 using Travel_list_API.Models;
+using Travel_list_API.Models.IRepositories;
 
 namespace Travel_list_API
 {
@@ -33,6 +28,7 @@ namespace Travel_list_API
 
             //services.AddScoped<TravelListDataInit>();
             services.AddScoped<ITravelListRepository, TravelListRepository>();
+            services.AddScoped<IClientRepository, ClientRepository>();
             services.AddDbContext<TravelListContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
 
             //services.AddDbContext<TravelListContext>(Options =>
