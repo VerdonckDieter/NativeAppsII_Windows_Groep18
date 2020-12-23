c658b49a49e981cf2049332933f77c6f44b9fe7a
Dieter Verdonck <dieter.verdonck@student.hogent.be>
2020-12-23 19:31:05 +0100

    Add Navigation items and NavigationViewmodel
    Co-authored-by: helderweirtthibault <thibault.helderweirt@student.hogent.be>

---
+++ NativeAppsII_Windows_Groep18/ViewModel/NavigationViewModel.cs
@@ -0,0 +1,14 @@
+﻿using NativeAppsII_Windows_Groep18.Model;
+using NativeAppsII_Windows_Groep18.Model.Singleton;
+
+namespace NativeAppsII_Windows_Groep18.ViewModel
+{
+    public class NavigationViewModel
+    {
+        public Client Client { get; set; }
+        public NavigationViewModel()
+        {
+            Client = ClientSingleton.Instance.Client;
+        }
+    }
+}
