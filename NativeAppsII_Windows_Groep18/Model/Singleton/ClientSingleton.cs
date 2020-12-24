80ef7aaa508036cb2f1ab55de02dbe3c5a9914fa
Dieter Verdonck <dieter.verdonck@student.hogent.be>
2020-12-24 21:53:28 +0100

    Implement INotifyPropertyChanged in ClientSingleton
    Co-authored-by: helderweirtthibault <thibault.helderweirt@student.hogent.be>

--- NativeAppsII_Windows_Groep18/Model/Singleton/ClientSingleton.cs
+++ NativeAppsII_Windows_Groep18/Model/Singleton/ClientSingleton.cs
@@ -1,12 +1,11 @@
-﻿
+﻿using System.ComponentModel;
+
 namespace NativeAppsII_Windows_Groep18.Model.Singleton
 {
-    public class ClientSingleton
+    public class ClientSingleton : INotifyPropertyChanged
     {
         private static ClientSingleton _instance;
-        public Client Client { get; set; }
-        private ClientSingleton() {}
-        //public void SetClient(Client client) => Client = client;
+        private ClientSingleton() { }
         public static ClientSingleton Instance
         {
             get
@@ -17,6 +16,21 @@ namespace NativeAppsII_Windows_Groep18.Model.Singleton
                 }
                 return _instance;
             }
+        }
+        private Client _client;
+        public Client Client
+        {
+            get { return _client; }
+            set
+            {
+                _client = value;
+                NotifyPropertyChanged("Client");
+            }
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void NotifyPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
     }
 }
