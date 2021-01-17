2093f316c1327dcb5a9a445dba0a666374ec9f6b
Dieter Verdonck <dieter.verdonck@student.hogent.be>
2021-01-17 01:36:42 +0100

    Add icons to buttons, remove unused imports
    Co-authored-by: helderweirtthibault <thibault.helderweirt@student.hogent.be>

--- NativeAppsII_Windows_Groep18/View/MainPage.xaml.cs
+++ NativeAppsII_Windows_Groep18/View/MainPage.xaml.cs
@@ -3,16 +3,8 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -25,18 +17,18 @@ namespace NativeAppsII_Windows_Groep18
     {
         public MainPage()
         {
-            this.InitializeComponent();
+            InitializeComponent();
            
         }
 
         private void LoginUser(object sender, RoutedEventArgs e)
         {
-            this.Frame.Navigate(typeof(Login));
+            Frame.Navigate(typeof(Login));
         }
 
         private void RegisterUser(object sender, RoutedEventArgs e)
         {
-            this.Frame.Navigate(typeof(Register));
+            Frame.Navigate(typeof(Register));
         }
     }
 }
