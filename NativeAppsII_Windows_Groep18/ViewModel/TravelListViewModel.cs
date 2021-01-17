109248386658b52bb98fee1d7dddeb5a005f0129
Dieter Verdonck <dieter.verdonck@student.hogent.be>
2021-01-17 01:35:00 +0100

    Add update travel list method, clean up code
    Co-authored-by: helderweirtthibault <thibault.helderweirt@student.hogent.be>

--- NativeAppsII_Windows_Groep18/ViewModel/TravelListViewModel.cs
+++ NativeAppsII_Windows_Groep18/ViewModel/TravelListViewModel.cs
@@ -38,11 +38,10 @@ namespace NativeAppsII_Windows_Groep18.ViewModel
             {
                 TravelLists.Add(travelList);
             }
-            GroupBy();
-            InitializeProgress();
+            InitializeTravelListProperties();
         }
 
-        public async System.Threading.Tasks.Task AddTravelList(String name, DateTime startdate, DateTime enddate, 
+        public async System.Threading.Tasks.Task AddTravelList(String name, DateTime startdate, DateTime enddate,
             double startLatitude, double startLongitude, double endLatitude, double endLongitude, List<Item> items = null, List<Model.Task> tasks = null)
         {
             var itinerary = new Itinerary() { StartLatitude = startLatitude, StartLongitude = startLongitude, EndLatitude = endLatitude, EndLongitude = endLongitude };
@@ -56,22 +55,73 @@ namespace NativeAppsII_Windows_Groep18.ViewModel
                 Itinerary = itinerary
             };
             var travelListJson = JsonConvert.SerializeObject(travelList);
-
             HttpClient client = new HttpClient();
-            var res = await client.PostAsync(new Uri(String.Format("http://localhost:5000/api/travelLists/{0}", ClientSingleton.Instance.Client.Id)), new HttpStringContent(travelListJson, Windows.Storage.Streams.UnicodeEncoding.Utf8, "application/json"));
-
-            if (res.IsSuccessStatusCode)
+            try
             {
-                TravelLists.Add(JsonConvert.DeserializeObject<TravelList>(res.Content.ToString()));
+                var res = await client.PostAsync(new Uri(String.Format("http://localhost:5000/api/travelLists/{0}", ClientSingleton.Instance.Client.Id)), new HttpStringContent(travelListJson, Windows.Storage.Streams.UnicodeEncoding.Utf8, "application/json"));
+
+                if (res.IsSuccessStatusCode)
+                {
+                    TravelLists.Add(JsonConvert.DeserializeObject<TravelList>(res.Content.ToString()));
+                }
+            }
+            catch (System.Net.Http.HttpRequestException)
+            {
+                throw new Exception("Connection failed");
+            }
+            catch (Exception)
+            {
+                throw;
             }
         }
 
-        public void GroupBy()
+        public async System.Threading.Tasks.Task UpdateTravelList(TravelList travelList)
+        {
+            HttpClient client = new HttpClient();
+            travelList.Items = new ObservableCollection<Item>(RetrieveItemsFromDictionary(travelList));
+            var travelListDto = new TravelListDTO()
+            {
+                Id = travelList.Id,
+                Name = travelList.Name,
+                StartDate = travelList.StartDate,
+                EndDate = travelList.EndDate,
+                Items = travelList.Items.ToList()
+            };
+            var updateTravelListJson = JsonConvert.SerializeObject(travelListDto);
+            try
+            {
+                var res = await client.PutAsync(new Uri(String.Format("http://localhost:5000/api/travelLists/{0}/travelLists/{1}", ClientSingleton.Instance.Client.Id, travelList.Id)), new HttpStringContent(updateTravelListJson, Windows.Storage.Streams.UnicodeEncoding.Utf8, "application/json"));
+                if (res.IsSuccessStatusCode)
+                {
+                }
+            }
+            catch (System.Net.Http.HttpRequestException)
+            {
+                throw new Exception("Connection failed");
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public void InitializeTravelListProperties()
         {
             foreach (TravelList t in TravelLists)
             {
                 t.ItemsGrouped = t.Items.GroupBy(i => i.Category).ToDictionary(i => i.Key, i => i.ToList());
+                t.UpdateProgress();
             }
+        }
+
+        public List<Item> RetrieveItemsFromDictionary(TravelList travelList)
+        {
+            List<Item> items = new List<Item>();
+            foreach (var i in travelList.ItemsGrouped)
+            {
+                items.AddRange(i.Value);
+            }
+            return items;
         }
 
         public void InitializeCategories()
@@ -83,14 +133,6 @@ namespace NativeAppsII_Windows_Groep18.ViewModel
                 new Category() { Name = "Bad" },
                 new Category() { Name = "Kledij" }
             };
-        }
-
-        public void InitializeProgress()
-        {
-            foreach (TravelList t in TravelLists)
-            {
-                t.UpdateProgress();
-            }
         }
         #endregion
     }
