b5fe116a40f4bb54147213eb10f22d9a7008ea30
Dieter Verdonck <dieter.verdonck@student.hogent.be>
2021-01-17 01:37:44 +0100

    Add different visual states, remove unused imports
    Co-authored-by: helderweirtthibault <thibault.helderweirt@student.hogent.be>

--- NativeAppsII_Windows_Groep18/View/Itinerary.xaml.cs
+++ NativeAppsII_Windows_Groep18/View/Itinerary.xaml.cs
@@ -1,22 +1,11 @@
 ﻿using NativeAppsII_Windows_Groep18.DataModel;
 using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Devices.Geolocation;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
 using Windows.Services.Maps;
 using Windows.UI;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Maps;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -41,10 +30,8 @@ namespace NativeAppsII_Windows_Groep18.View
         private async void ShowRouteOnMap(double startLatitude, double startLongitude, double endLatitude, double endLongitude)
         {
             TravelMap.Routes.Clear();
-            //Start location
-            BasicGeoposition startLocation = new BasicGeoposition() { Latitude = startLatitude, Longitude = startLongitude };
 
-            //End location
+            BasicGeoposition startLocation = new BasicGeoposition() { Latitude = startLatitude, Longitude = startLongitude };
             BasicGeoposition endLocation = new BasicGeoposition() { Latitude = endLatitude, Longitude = endLongitude };
 
             // Get the route between the points.
