eba1712d04c5a004c84ab98b617418ea539a046a
Dieter Verdonck <dieter.verdonck@student.hogent.be>
2021-06-05 02:17:52 +0200

    Refactor controllers

--- Travel_list_API/Controllers/TravelListsController.cs
+++ Travel_list_API/Controllers/TravelListsController.cs
@@ -1,86 +1,86 @@
-﻿using System.Collections.Generic;
-using Microsoft.AspNetCore.Mvc;
-using Travel_list_API.Models;
-using Travel_list_API.Models.DTO;
+﻿//using System.Collections.Generic;
+//using Microsoft.AspNetCore.Mvc;
+//using Travel_list_API.Models;
+//using Travel_list_API.Models.DTO;
 
-namespace Travel_list_API.Controllers
-{
-    [Route("api/[controller]")]
-    [ApiController]
-    public class TravelListsController : ControllerBase
-    {
-        private readonly ITravelListRepository _travelListRepository;
-        public TravelListsController(ITravelListRepository travelListRepository)
-        {
-            _travelListRepository = travelListRepository;
-        }
+//namespace Travel_list_API.Controllers
+//{
+//    [Route("api/[controller]")]
+//    [ApiController]
+//    public class TravelListsController : ControllerBase
+//    {
+//        private readonly ITravelListRepository _travelListRepository;
+//        public TravelListsController(ITravelListRepository travelListRepository)
+//        {
+//            _travelListRepository = travelListRepository;
+//        }
 
-        [HttpGet("{clientId}/travelLists")]
-        public IEnumerable<TravelList> GetTravelLists(int clientId)
-        {
-            return _travelListRepository.GetTravelLists(clientId);
-        }
+//        [HttpGet("{clientId}/travelLists")]
+//        public IEnumerable<Trip> GetTravelLists(int clientId)
+//        {
+//            return _travelListRepository.GetTravelLists(clientId);
+//        }
 
-        [HttpGet("{clientId}/travelLists/{travelListId}")]
-        public TravelList GetTravelListById(int clientId, int travelListId)
-        {
-            return _travelListRepository.GetTravelListById(clientId, travelListId);
-        }
+//        [HttpGet("{clientId}/travelLists/{travelListId}")]
+//        public Trip GetTravelListById(int clientId, int travelListId)
+//        {
+//            return _travelListRepository.GetTravelListById(clientId, travelListId);
+//        }
 
-        [HttpPost("{clientId}")]
-        public ActionResult<TravelList> PostTravelList(int clientId, TravelListDTO travelListDTO)
-        {
-            var travelList = new TravelList()
-            {
-                Id = travelListDTO.Id,
-                ClientId = clientId,
-                Name = travelListDTO.Name,
-                StartDate = travelListDTO.StartDate,
-                EndDate = travelListDTO.EndDate,
-                Items = travelListDTO.Items,
-                Tasks = travelListDTO.Tasks,
-                Categories = travelListDTO.Categories,
-                Itinerary = travelListDTO.Itinerary
-            };
-            _travelListRepository.AddTravelList(clientId, travelList);
-            _travelListRepository.SaveChanges();
+//        [HttpPost("{clientId}")]
+//        public ActionResult<Trip> PostTravelList(int clientId, TripDTO travelListDTO)
+//        {
+//            var travelList = new Trip()
+//            {
+//                Id = travelListDTO.Id,
+//                ClientId = clientId,
+//                Name = travelListDTO.Name,
+//                StartDate = travelListDTO.StartDate,
+//                EndDate = travelListDTO.EndDate,
+//                Items = travelListDTO.Items,
+//                Tasks = travelListDTO.Tasks,
+//                Categories = travelListDTO.Categories,
+//                Itinerary = travelListDTO.Itinerary
+//            };
+//            _travelListRepository.AddTravelList(clientId, travelList);
+//            _travelListRepository.SaveChanges();
 
-            return CreatedAtAction(nameof(GetTravelListById), new { clientId, travelListId = travelList.Id }, travelList);
-        }
+//            return CreatedAtAction(nameof(GetTravelListById), new { clientId, travelListId = travelList.Id }, travelList);
+//        }
 
-        [HttpPut("{clientId}/travelLists/{travelListId}")]
-        public IActionResult PutTravelList(int clientId, int travelListId, TravelListDTO travelListDTO)
-        {
-            if (travelListId != travelListDTO.Id)
-            {
-                return BadRequest();
-            }
-            var travelList = new TravelList()
-            {
-                Id = travelListDTO.Id,
-                ClientId = clientId,
-                Name = travelListDTO.Name,
-                StartDate = travelListDTO.StartDate,
-                EndDate = travelListDTO.EndDate,
-                Items = travelListDTO.Items
-            };
-            _travelListRepository.UpdateTravelList(travelList);
-            _travelListRepository.SaveChanges();
+//        [HttpPut("{clientId}/travelLists/{travelListId}")]
+//        public IActionResult PutTravelList(int clientId, int travelListId, TripDTO travelListDTO)
+//        {
+//            if (travelListId != travelListDTO.Id)
+//            {
+//                return BadRequest();
+//            }
+//            var travelList = new Trip()
+//            {
+//                Id = travelListDTO.Id,
+//                ClientId = clientId,
+//                Name = travelListDTO.Name,
+//                StartDate = travelListDTO.StartDate,
+//                EndDate = travelListDTO.EndDate,
+//                Items = travelListDTO.Items
+//            };
+//            _travelListRepository.UpdateTravelList(travelList);
+//            _travelListRepository.SaveChanges();
 
-            return NoContent();
-        }
+//            return NoContent();
+//        }
 
-        [HttpDelete("{clientId}/travelLists/{travelListId}")]
-        public ActionResult<TravelList> DeleteTravelList(int clientId, int travelListId)
-        {
-            TravelList travelList = _travelListRepository.GetTravelListById(clientId, travelListId);
-            if (travelList == null)
-            {
-                return NotFound();
-            }
-            _travelListRepository.DeleteTravelList(clientId, travelList);
-            _travelListRepository.SaveChanges();
-            return travelList;
-        }
-    }
-}
+//        [HttpDelete("{clientId}/travelLists/{travelListId}")]
+//        public ActionResult<Trip> DeleteTravelList(int clientId, int travelListId)
+//        {
+//            Trip travelList = _travelListRepository.GetTravelListById(clientId, travelListId);
+//            if (travelList == null)
+//            {
+//                return NotFound();
+//            }
+//            _travelListRepository.DeleteTravelList(clientId, travelList);
+//            _travelListRepository.SaveChanges();
+//            return travelList;
+//        }
+//    }
+//}
---
+++ Travel_list_API/Controllers/UserController.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using Travel_list_API.Data.Repositories.IRepositories;
+using Travel_list_API.Models;
+using Travel_list_API.Models.DTO;
+using Travel_list_API.Models.DTO.Authentication;
+
+namespace Travel_list_API.Controllers
+{
+    /// <summary>
+    /// Contains methods for interacting with user data.
+    /// </summary>
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly IUserRepository _userRepository;
+        private readonly SignInManager<IdentityUser> _signInManager;
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly IConfiguration _config;
+
+        public UserController(IUserRepository clientRepository, SignInManager<IdentityUser> signInManager, UserManager<IdentityUser> userManager, IConfiguration config)
+        {
+            _userRepository = clientRepository;
+            _signInManager = signInManager;
+            _userManager = userManager;
+            _config = config;
+        }
+
+        /// <summary>
+        /// Gets all users.
+        /// </summary>
+        [HttpGet]
+        //[Authorize(Policy = "Admin")]
+        public async Task<ActionResult<IEnumerable<UserDTO>>> GetUsersAsync()
+        {
+            try
+            {
+                return Ok(await _userRepository.GetUsersAsync());
+            }
+            catch (Exception)
+            {
+                return NotFound();
+            }
+        }
+
+        /// <summary>
+        /// Gets the current logged in user.
+        /// </summary>
+        [HttpGet("{id}")]
+        public async Task<ActionResult<UserDTO>> GetUserAsync(int id)
+        {
+            try
+            {
+                return Ok(await _userRepository.GetUserAsync(User.Identity.Name, id));
+            }
+            catch (Exception)
+            {
+                return NotFound();
+            }
+        }
+
+        /// <summary>
+        /// Registers a new user.
+        /// </summary>
+        [HttpPost("register")]
+        public async Task<ActionResult> Register([FromBody]RegisterDTO model)
+        {
+            var identityUser = new IdentityUser { UserName = model.Email, Email = model.Email };
+            var user = new User()
+            {
+                Email = model.Email,
+                FirstName = model.FirstName,
+                LastName = model.LastName,
+                BirthDate = model.BirthDate
+            };
+            var result = await _userManager.CreateAsync(identityUser, model.Password);
+            if (result.Succeeded)
+            {
+                result = await _userManager.AddClaimAsync(identityUser, new Claim(ClaimTypes.Role, "client"));
+
+                if (result.Succeeded && await _userRepository.UpsertUserAsync(user))
+                    return Ok();
+            }
+            return BadRequest();
+        }
+
+        /// <summary>
+        /// Logs in a user.
+        /// </summary>
+        [AllowAnonymous]
+        [HttpPost("login")]
+        public async Task<ActionResult<string>> Login([FromBody] LoginDTO model)
+        {
+            var user = await _userManager.FindByEmailAsync(model.Email);
+            if (user != null && (await _signInManager.CheckPasswordSignInAsync(user, model.Password, false)).Succeeded)
+                return Created("", await GetToken(user));
+            return BadRequest();
+        }
+
+        /// <summary>
+        /// Creates a JWT token based on the provided IdentityUser and append claim information.
+        /// </summary>
+        private async Task<string> GetToken(IdentityUser user)
+        {
+            var claims = new List<Claim> { new Claim(JwtRegisteredClaimNames.UniqueName, user.UserName) };
+            claims.AddRange((await _userManager.GetClaimsAsync(user)));
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Tokens:Key"]));
+
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(
+                null, null,
+                claims,
+                expires: DateTime.Now.AddHours(2),
+                signingCredentials: creds);
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+    }
+}
