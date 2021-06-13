dfce9f7776493b1e7a80a0bf3810aa43a8d98963
Dieter Verdonck <dieter.verdonck@student.hogent.be>
2021-06-13 19:13:47 +0200

    Add controllers for Category, Chore and Itinerary

---
+++ Travel_list_API/Controllers/CategoryController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using Travel_list_API.Models;
+using Travel_list_API.Models.DTO;
+using Travel_list_API.Models.IRepositories;
+
+namespace Travel_list_API.Controllers
+{
+    /// <summary>
+    /// Contains methods for interacting with category data.
+    /// </summary>
+    [Route("api/[controller]")]
+    [ApiController]
+    [ApiConventionType(typeof(DefaultApiConventions))]
+    [Authorize(Policy = "User")]
+    public class CategoryController : ControllerBase
+    {
+        private readonly ICategoryRepository _categoryRepository;
+
+        public CategoryController(ICategoryRepository categoryRepository) => _categoryRepository = categoryRepository;
+
+        /// <summary>
+        /// Creates a new category or updates an existing one.
+        /// </summary>
+        [HttpPost("{tripId}")]
+        public async Task<ActionResult> PostCategory(int tripId, CategoryDTO categoryDTO)
+        {
+            var category = new Category()
+            {
+                Id = categoryDTO.Id,
+                Name = categoryDTO.Name,
+                Items = categoryDTO.Items
+            };
+            return Ok(await _categoryRepository.UpsertCategoryAsync(tripId, category));
+        }
+
+        /// <summary>
+        /// Deletes a category.
+        /// </summary>
+        [HttpDelete("{tripId}/{categoryId}")]
+        public async Task<ActionResult> DeleteCategory(int tripId, int categoryId)
+        {
+            return Ok(await _categoryRepository.DeleteCategoryAsync(tripId, categoryId));
+        }
+    }
+}
---
+++ Travel_list_API/Controllers/ChoreController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using Travel_list_API.Models;
+using Travel_list_API.Models.DTO;
+using Travel_list_API.Models.IRepositories;
+
+namespace Travel_list_API.Controllers
+{
+    /// <summary>
+    /// Contains methods for interacting with chores data.
+    /// </summary>
+    [Route("api/[controller]")]
+    [ApiController]
+    [ApiConventionType(typeof(DefaultApiConventions))]
+    [Authorize(Policy = "User")]
+    public class ChoreController : ControllerBase
+    {
+        private readonly IChoreRepository _choreRepository;
+
+        public ChoreController(IChoreRepository choreRepository) => _choreRepository = choreRepository;
+
+        /// <summary>
+        /// Creates a new chore or updates an existing one.
+        /// </summary>
+        [HttpPost("{tripId}")]
+        public async Task<ActionResult> PostChore(int tripId, ChoreDTO choreDTO)
+        {
+            var chore = new Chore()
+            {
+                Id = choreDTO.Id,
+                Name = choreDTO.Name,
+                Completed = choreDTO.Completed
+            };
+            return Ok(await _choreRepository.UpsertChoreAsync(tripId, chore));
+        }
+
+        /// <summary>
+        /// Deletes a chore.
+        /// </summary>
+        [HttpDelete("{tripId}/{choreId}")]
+        public async Task<ActionResult> DeleteChore(int tripId, int choreId)
+        {
+            return Ok(await _choreRepository.DeleteChoreAsync(tripId, choreId));
+        }
+    }
+}
