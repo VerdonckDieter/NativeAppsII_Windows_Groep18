33cc2682b48851955013605c115f09b894dbadcf
Dieter Verdonck <dieter.verdonck@student.hogent.be>
2021-06-13 19:13:14 +0200

    Replace TravelListController with TripController

---
+++ Travel_list_API/Controllers/TripController.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Travel_list_API.Models;
+using Travel_list_API.Models.DTO;
+using Travel_list_API.Models.IRepositories;
+
+namespace Travel_list_API.Controllers
+{
+    /// <summary>
+    /// Contains methods for interacting with trip data.
+    /// </summary>
+    [Route("api/[controller]")]
+    [ApiController]
+    [ApiConventionType(typeof(DefaultApiConventions))]
+    [Authorize(Policy = "User")]
+    public class TripController : ControllerBase
+    {
+        private readonly ITripRepository _tripRepository;
+        public TripController(ITripRepository tripRepository) => _tripRepository = tripRepository;
+
+        /// <summary>
+        /// Gets all trips of the logged in user.
+        /// </summary>
+        [HttpGet("GetTrips")]
+        public async Task<ActionResult<IEnumerable<Trip>>> GetTripsAsync()
+        {
+            try
+            {
+                return Ok(await _tripRepository.GetTripsAsync(User.Identity.Name));
+            }
+            catch (Exception)
+            {
+                return NotFound();
+            }
+        }
+
+        /// <summary>
+        /// Gets the trip with the given id of the logged in user.
+        /// </summary>
+        [HttpGet("{tripId}")]
+        public async Task<ActionResult<Trip>> GetTripAsync(int tripId)
+        {
+            try
+            {
+                return Ok(await _tripRepository.GetTripAsync(User.Identity.Name, tripId));
+            }
+            catch (Exception)
+            {
+                return NotFound();
+            }
+        }
+
+        /// <summary>
+        /// Creates a new trip or updates an existing one.
+        /// </summary>
+        [HttpPost]
+        public async Task<ActionResult> PostTrip(TripDTO tripDTO)
+        {
+            var trip = new Trip()
+            {
+                Id = tripDTO.Id,
+                Name = tripDTO.Name,
+                StartDate = tripDTO.StartDate,
+                EndDate = tripDTO.EndDate,
+                Chores = tripDTO.Chores,
+                Categories = tripDTO.Categories,
+                Itineraries = tripDTO.Itineraries
+            };
+            return Ok(await _tripRepository.UpsertTripAsync(User.Identity.Name, trip));
+        }
+
+        /// <summary>
+        /// Deletes a trip.
+        /// </summary>
+        [HttpDelete("{tripId}")]
+        public async Task<ActionResult> DeleteTrip(int tripId)
+        {
+            return Ok(await _tripRepository.DeleteTripAsync(User.Identity.Name, tripId));
+        }
+    }
+}
