4806cb75ec1f377e58bc6b696126821fa1dde242
Dieter Verdonck <dieter.verdonck@student.hogent.be>
2021-06-13 19:16:23 +0200

    Add IRepositories for Category, Chore, Itinerary, Trip and User

---
+++ Travel_list_API/Models/IRepositories/IChoreRepository.cs
@@ -0,0 +1,21 @@
+﻿using System.Threading.Tasks;
+
+namespace Travel_list_API.Models.IRepositories
+{
+    /// <summary>
+    /// Defines methods for interacting with the chores backend.
+    /// </summary>
+    public interface IChoreRepository
+    {
+        /// <summary>
+        /// Adds a new chore if the chore does not exist, updates the 
+        /// existing chore otherwise.
+        /// </summary>
+        Task<Chore> UpsertChoreAsync(int tripId, Chore chore);
+
+        /// <summary>
+        /// Deletes a chore.
+        /// </summary>
+        Task<bool> DeleteChoreAsync(int tripId, int choreId);
+    }
+}
---
+++ Travel_list_API/Models/IRepositories/IItineraryRepository.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Travel_list_API.Models.IRepositories
+{
+    /// <summary>
+    /// Defines methods for interacting with the itineraries backend.
+    /// </summary>
+    public interface IItineraryRepository
+    {
+        /// <summary>
+        /// Returns all itineraries associated with the given tripId. 
+        /// </summary>
+        Task<IEnumerable<Itinerary>> GetItinerariesAsync(int tripId);
+
+        /// <summary>
+        /// Returns the itinerary with the given id associated with the given tripId. 
+        /// </summary>
+        Task<Itinerary> GetItineraryAsync(int tripId, int itineraryId);
+
+        /// <summary>
+        /// Adds a new itinerary if the itinerary does not exist, updates the 
+        /// existing itinerary otherwise.
+        /// </summary>
+        Task<Itinerary> UpsertItineraryAsync(int tripId, Itinerary itinerary);
+
+        /// <summary>
+        /// Deletes an itinerary.
+        /// </summary>
+        Task<bool> DeleteItineraryAsync(int tripId, int itineraryId);
+    }
+}
