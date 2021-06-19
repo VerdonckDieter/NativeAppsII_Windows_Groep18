4e046c928d27e2e4944bef0c89375b19a9f0b427
Dieter Verdonck <dieter.verdonck@student.hogent.be>
2021-06-20 01:52:04 +0200

    Add user service, move globals

---
+++ NativeAppsII_Windows_Groep18/Services/IServices/IUserService.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks;
+
+namespace NativeAppsII_Windows_Groep18.Services.IServices
+{
+    public interface IUserService
+    {
+        Task<string> Login(string email, string password);
+
+        Task<string> Register(string email, string password, string firstname, string lastname);
+
+        Task<string> CheckAvailableUsername(string email);
+    }
+}
---
+++ NativeAppsII_Windows_Groep18/Services/Instances/UserService.cs
@@ -0,0 +1,86 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Threading.Tasks;
+using Windows.Web.Http;
+using Windows.Storage.Streams;
+using NativeAppsII_Windows_Groep18.Utility;
+using NativeAppsII_Windows_Groep18.Services.IServices;
+
+namespace NativeAppsII_Windows_Groep18.Services.Instances
+{
+    public class UserService : IUserService
+    {
+        private readonly HttpClient _httpClient;
+
+        public UserService()
+        {
+            _httpClient = new HttpClient();
+        }
+
+        public async Task<string> Login(string email, string password)
+        {
+            try
+            {
+                var json = JsonConvert.SerializeObject(new
+                {
+                    email,
+                    password
+                });
+                var result = await _httpClient.PostAsync(new Uri($"{Globals.BASE_URL}/User/Login"),
+                    new HttpStringContent(json, UnicodeEncoding.Utf8, "application/json"));
+                if (result.IsSuccessStatusCode)
+                {
+                    Globals.LoggedInUser = await result.Content.ReadAsStringAsync();
+                    return "SUCCESS";
+                }
+                return "FAIL";
+            }
+            catch (Exception)
+            {
+                return "ERROR";
+            }
+        }
+
+        public async Task<string> Register(string email, string password, string firstname, string lastname)
+        {
+            try
+            {
+                var json = JsonConvert.SerializeObject(new
+                {
+                    email,
+                    password,
+                    firstname,
+                    lastname
+                });
+                var result = await _httpClient.PostAsync(new Uri($"{Globals.BASE_URL}/User/Register"),
+                    new HttpStringContent(json, UnicodeEncoding.Utf8, "application/json"));
+                if (result.IsSuccessStatusCode)
+                {
+                    return "SUCCESS";
+                }
+                return "FAIL";
+            }
+            catch (Exception)
+            {
+                return "ERROR";
+            }
+        }
+
+        public async Task<string> CheckAvailableUsername(string email)
+        {
+            try
+            {
+                var result = await _httpClient.GetAsync(new Uri($"{Globals.BASE_URL}/User/" + email));
+                if (result.IsSuccessStatusCode)
+                {
+                    return await result.Content.ReadAsStringAsync() == "true" ? "AVAILABLE" : "NOTAVAILABLE";
+                }
+                return "FAIL";
+            }
+            catch (Exception)
+            {
+                return "ERROR";
+            }
+        }
+    }
+}
