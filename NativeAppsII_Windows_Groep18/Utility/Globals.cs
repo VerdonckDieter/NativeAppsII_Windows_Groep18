b4daaff85ecf9f72078748d82e46d6458bc9775b
Dieter Verdonck <dieter.verdonck@student.hogent.be>
2021-06-20 01:52:15 +0200

    Add utility folder

---
+++ NativeAppsII_Windows_Groep18/Utility/Globals.cs
@@ -0,0 +1,17 @@
+﻿namespace NativeAppsII_Windows_Groep18.Utility
+{
+    public static class Globals
+    {
+        #region Properties
+        public static string LoggedInUser { get; set; }
+
+        public static string BASE_URL
+        {
+            get
+            {
+                return "https://localhost:5001/api";
+            }
+        }
+        #endregion   
+    }
+}
