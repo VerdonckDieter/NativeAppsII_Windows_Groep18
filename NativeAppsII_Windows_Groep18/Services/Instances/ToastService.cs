09736c75c9500201735e59d53f33d563431f81ba
Dieter Verdonck <dieter.verdonck@student.hogent.be>
2021-06-21 23:48:53 +0200

    Add toast service

---
+++ NativeAppsII_Windows_Groep18/Services/Instances/ToastService.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Toolkit.Uwp.Notifications;
+using Windows.UI.Notifications;
+
+namespace NativeAppsII_Windows_Groep18.Services.Instances
+{
+    public static class ToastService
+    {
+        public static void MakeToast(string text)
+        {
+            var content = new ToastContentBuilder()
+                .AddText(text)
+                .SetToastDuration(ToastDuration.Short)
+                .GetToastContent();
+            var notification = new ToastNotification(content.GetXml());
+            ToastNotificationManager.CreateToastNotifier().Show(notification);
+        }
+    }
+}
