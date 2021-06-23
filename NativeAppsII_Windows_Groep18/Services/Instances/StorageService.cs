df365c41cb4b0bdb5e64d33f7fe4b8a31ce9f25f
Dieter Verdonck <dieter.verdonck@student.hogent.be>
2021-06-23 22:00:56 +0200

    Add token storage service

---
+++ NativeAppsII_Windows_Groep18/Services/Instances/StorageService.cs
@@ -0,0 +1,21 @@
+﻿using Windows.Storage;
+
+namespace NativeAppsII_Windows_Groep18.Services.Instances
+{
+    public static class StorageService
+    {
+        public static void StoreToken(string token)
+        {
+            ApplicationData.Current.LocalSettings.Values["token"] = token;
+        }
+
+        public static string RetrieveToken()
+        {
+            if(ApplicationData.Current.LocalSettings.Values.TryGetValue("token", out object token))
+            {
+                return token.ToString();
+            }
+            return default;
+        }
+    }
+}
