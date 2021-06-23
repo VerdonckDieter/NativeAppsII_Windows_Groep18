739fba0e01e6446389273eb6afde55e71832a4c5
Dieter Verdonck <dieter.verdonck@student.hogent.be>
2021-06-23 22:01:51 +0200

    Add trip service interface

---
+++ NativeAppsII_Windows_Groep18/Services/IServices/ITripService.cs
@@ -0,0 +1,15 @@
+﻿using NativeAppsII_Windows_Groep18.Model;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace NativeAppsII_Windows_Groep18.Services.IServices
+{
+    public interface ITripService
+    {
+        Task<List<Trip>> GetTrips();
+
+        Task<Trip> GetTrip(int id);
+
+        Task<bool> DeleteTrip(int id);
+    }
+}
