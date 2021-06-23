0d4457df27ed3a796df4b67d020453cc1bcd4665
Dieter Verdonck <dieter.verdonck@student.hogent.be>
2021-06-23 22:01:10 +0200

    Add trip service

---
+++ NativeAppsII_Windows_Groep18/Services/Instances/TripService.cs
@@ -0,0 +1,43 @@
+﻿using NativeAppsII_Windows_Groep18.Model;
+using NativeAppsII_Windows_Groep18.Services.IServices;
+using NativeAppsII_Windows_Groep18.Utility;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Windows.Web.Http;
+using Windows.Web.Http.Headers;
+
+namespace NativeAppsII_Windows_Groep18.Services.Instances
+{
+    public class TripService : ITripService
+    {
+        private readonly HttpClient _httpClient;
+
+        public TripService()
+        {
+            _httpClient = new HttpClient();
+        }
+
+        public async Task<List<Trip>> GetTrips()
+        {
+            _httpClient.DefaultRequestHeaders.Authorization = new HttpCredentialsHeaderValue("Bearer", StorageService.RetrieveToken());
+            var json = await _httpClient.GetStringAsync(new Uri($"{Globals.BASE_URL}/Trip/GetTrips"));
+            return JsonConvert.DeserializeObject<List<Trip>>(json);
+        }
+
+        public async Task<Trip> GetTrip(int id)
+        {
+            _httpClient.DefaultRequestHeaders.Authorization = new HttpCredentialsHeaderValue("Bearer", StorageService.RetrieveToken());
+            var json = await _httpClient.GetStringAsync(new Uri($"{Globals.BASE_URL}/Trip/{id}"));
+            return JsonConvert.DeserializeObject<Trip>(json);
+        }
+
+        public async Task<bool> DeleteTrip(int id)
+        {
+            _httpClient.DefaultRequestHeaders.Authorization = new HttpCredentialsHeaderValue("Bearer", StorageService.RetrieveToken());
+            var json = await _httpClient.DeleteAsync(new Uri($"{Globals.BASE_URL}/Trip/{id}"));
+            return JsonConvert.DeserializeObject<bool>(await json.Content.ReadAsStringAsync());
+        }
+    }
+}
