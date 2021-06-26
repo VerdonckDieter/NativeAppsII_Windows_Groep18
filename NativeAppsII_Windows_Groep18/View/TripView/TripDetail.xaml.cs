cef8fd3f97c48f39a82707f136ad63f5fb713f4b
Dieter Verdonck <dieter.verdonck@student.hogent.be>
2021-06-26 18:41:47 +0200

    Add trip detail page

---
+++ NativeAppsII_Windows_Groep18/View/TripView/TripDetail.xaml.cs
@@ -0,0 +1,43 @@
+﻿using NativeAppsII_Windows_Groep18.ViewModel;
+using NativeAppsII_Windows_Groep18.Model;
+using Microsoft.Extensions.DependencyInjection;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace NativeAppsII_Windows_Groep18.View.TripView
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class TripDetail : Page
+    {
+        #region Properties
+        public TripDetailViewModel TripDetailViewModel { get; set; }
+
+        #endregion
+        #region Constructors
+        public TripDetail()
+        {
+            InitializeComponent();
+            TripDetailViewModel = App.Current.Services.GetService<TripDetailViewModel>();
+            DataContext = TripDetailViewModel;
+        }
+        #endregion
+
+        #region Methods
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+
+            TripDetailViewModel.Trip = (Trip)e.Parameter;
+
+            if(TripDetailViewModel.WeatherForecast == null)
+            {
+                TripDetailViewModel.GetWeatherForecast();
+            }
+        }
+        #endregion
+    }
+}
