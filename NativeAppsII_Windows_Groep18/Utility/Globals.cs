520f4e35c63be214366dca898c25a72ea55358bf
Dieter Verdonck <dieter.verdonck@student.hogent.be>
2021-06-26 18:40:48 +0200

    Add weather api to globals

--- NativeAppsII_Windows_Groep18/Utility/Globals.cs
+++ NativeAppsII_Windows_Groep18/Utility/Globals.cs
@@ -5,13 +5,9 @@
         #region Properties
         public static string LoggedInUser { get; set; }
 
-        public static string BASE_URL
-        {
-            get
-            {
-                return "https://localhost:5001/api";
-            }
-        }
+        public static string BASE_URL => "https://localhost:5001/api";
+
+        public static string API_KEY => "694e2daf71c2da8439ea1eb23570adb4";
         #endregion   
     }
 }
