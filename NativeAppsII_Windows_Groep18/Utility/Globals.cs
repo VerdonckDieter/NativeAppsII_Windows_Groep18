47d1f41457582b5ac663e429de84bc2c52c86a32
Dieter Verdonck <dieter.verdonck@student.hogent.be>
2021-07-13 22:24:11 +0200

    Remove logged in user property

--- NativeAppsII_Windows_Groep18/Utility/Globals.cs
+++ NativeAppsII_Windows_Groep18/Utility/Globals.cs
@@ -3,8 +3,6 @@
     public static class Globals
     {
         #region Properties
-        public static string LoggedInUser { get; set; }
-
         public static string BASE_URL => "https://localhost:5001/api";
 
         public static string API_KEY => "694e2daf71c2da8439ea1eb23570adb4";
