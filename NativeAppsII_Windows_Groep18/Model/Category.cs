0be3887acb285db655ad7200a39aaf1fea9bfd0b
Dieter Verdonck <dieter.verdonck@student.hogent.be>
2021-07-13 22:23:54 +0200

    Add items added property to category

--- NativeAppsII_Windows_Groep18/Model/Category.cs
+++ NativeAppsII_Windows_Groep18/Model/Category.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.ObjectModel;
+using System.Linq;
 
 namespace NativeAppsII_Windows_Groep18.Model
 {
@@ -22,6 +23,11 @@ namespace NativeAppsII_Windows_Groep18.Model
         /// Gets or sets the category's items.
         /// </summary>
         public ObservableCollection<Item> Items { get; set; }
+
+        /// <summary>
+        /// Gets the category's added items.
+        /// </summary>
+        public int ItemsAdded => Items.Count(i => i.Added);
         #endregion
 
         #region Constructors
