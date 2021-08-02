c6ac1427716bdfde12d248b72e4110bc49c4df70
Dieter Verdonck <dieter.verdonck@student.hogent.be>
2021-08-02 23:22:57 +0200

    Add map token

--- NativeAppsII_Windows_Groep18/Utility/Globals.cs
+++ NativeAppsII_Windows_Groep18/Utility/Globals.cs
@@ -4,8 +4,8 @@
     {
         #region Properties
         public static string BASE_URL => "https://localhost:5001/api";
-
         public static string API_KEY => "694e2daf71c2da8439ea1eb23570adb4";
-        #endregion   
+        public static string MAP_TOKEN => "kiMTfBjS18oItbEwSQsi~V30ULXHdubcpoIgp7E9GjA~AhlM7Rp6OqMz_O06wAnydivBQG4XpUEUJKwS9nPg-y5tajWHqTi5VJ9fnOUiUQTD";
+        #endregion
     }
 }
