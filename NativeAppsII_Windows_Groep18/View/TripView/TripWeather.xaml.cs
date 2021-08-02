f36cdf9e4e44299cc8f6b183c0aa216b7a897835
Thibault Helderweirt <thibault.helderweirt@student.hogent.be>
2021-08-02 16:21:54 +0200

    Add TripWeather view code

---
+++ NativeAppsII_Windows_Groep18/View/TripView/TripWeather.xaml.cs
@@ -0,0 +1,43 @@
+﻿using NativeAppsII_Windows_Groep18.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace NativeAppsII_Windows_Groep18.View.TripView
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class TripWeather : Page
+    {
+
+        public TripDetailViewModel TripDetailViewModel { get; set; }
+
+        public TripWeather()
+        {
+            this.InitializeComponent();
+            DataContext = TripDetailViewModel;            
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+            TripDetailViewModel = (TripDetailViewModel)e.Parameter;
+        }
+
+    }
+}
