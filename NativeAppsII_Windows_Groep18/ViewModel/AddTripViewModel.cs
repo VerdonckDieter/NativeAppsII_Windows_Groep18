3914cb3e369196ffb48a58699bbf22599aac5ba7
Dieter Verdonck <dieter.verdonck@student.hogent.be>
2021-08-15 02:26:11 +0200

    Add regions

--- NativeAppsII_Windows_Groep18/ViewModel/AddTripViewModel.cs
+++ NativeAppsII_Windows_Groep18/ViewModel/AddTripViewModel.cs
@@ -7,17 +7,23 @@ namespace NativeAppsII_Windows_Groep18.ViewModel
 {
     public class AddTripViewModel
     {
+        #region Fields
         private readonly ITripService _tripService;
+        #endregion
 
+        #region Constructors
         public AddTripViewModel(ITripService tripService)
         {
             _tripService = tripService;
         }
+        #endregion
 
+        #region Methods
         public async Task<Trip> AddTrip(string name, string location, DateTime startDate, DateTime endDate)
         {
             var trip = new Trip() { Name = name, Location = location, StartDate = startDate, EndDate = endDate };
             return await _tripService.UpsertTrip(trip);
-        }
+        } 
+        #endregion
     }
 }
